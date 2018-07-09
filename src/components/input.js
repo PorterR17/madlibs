b50bde44f0082256881f3371eb6783008dfac74a
Porter Roth <porterroth17@gmail.com>
2018-07-09 10:55:55 -0600

    add real time word replace function

--- src/components/input.js
+++ src/components/input.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Input = ({title, state, name}, onChange) => {
 	return (
-		<div key={state} className="input">
+		<div key={name} className="input">
 			<input name={name} value={state} onChange={onChange}/>
 			<label>{title}</label>
 		</div>
