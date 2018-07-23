8143e5ef789b8f838f0f833cd3514205bac01573
Porter Roth <porterroth17@gmail.com>
2018-07-23 10:56:29 -0600

    styled generate madlib and clear form buttons

--- src/components/content.js
+++ src/components/content.js
@@ -5,25 +5,25 @@ class Content extends Component {
 		const data = this.props.data;
 		return (
 			<div className="card__content">
-			<p>
-				Ladies and gentlemen, this is 
-				<b>{data.color}</b> Barber, your sportscaster, bringing you the last inning of the game between the Cleveland 
-				<b>{data.pluralNoun}</b> and the 
-				<b>{data.adjectiveOne}</b> Yankees. 
-				<b>{data.celebOne}</b> is pitching for the Yankees. Here's the pitch! It's a low 
-				<b>{data.adjectiveTwo}</b> ball that just cut the inside of the 
-				<b>{data.nounOne}</b> for a strike. That makes the count 
-				<b>{data.numberOne} </b>strikes and 
-				<b>{data.numberTwo}</b> balls. Now there's the next pitch. The batter swings and connects. It's a long, high 
-				<b>{data.nounTwo}</b> out to 
-				<b>{data.adjectiveThree}</b> field. But 
-				<b>{data.celebTwo}</b> is coming up fast and has it for the second out. The next batter up is 
-				<b>{data.celebThree}</b>, the Cleveland 
-				<b>{data.adjectiveFor}</b>-stop. Here's the pitch... and it's hit... a short ground ball to third 
-				<b>{data.nounThree}</b>. 
-				<b>{data.celebFour}</b> scoops it up and throws it to first base for an out, and the game is over. And the Yankees move into second place in the 
-				<b>{data.adjectiveFive}</b> League! 
-			</p>
+				<p>
+					Ladies and gentlemen, this is 
+					<b>{data.color}</b> Barber, your sportscaster, bringing you the last inning of the game between the Cleveland 
+					<b>{data.pluralNoun}</b> and the 
+					<b>{data.adjectiveOne}</b> Yankees. 
+					<b>{data.celebOne}</b> is pitching for the Yankees. Here's the pitch! It's a low 
+					<b>{data.adjectiveTwo}</b> ball that just cut the inside of the 
+					<b>{data.nounOne}</b> for a strike. That makes the count 
+					<b>{data.numberOne} </b>strikes and 
+					<b>{data.numberTwo}</b> balls. Now there's the next pitch. The batter swings and connects. It's a long, high 
+					<b>{data.nounTwo}</b> out to 
+					<b>{data.adjectiveThree}</b> field. But 
+					<b>{data.celebTwo}</b> is coming up fast and has it for the second out. The next batter up is 
+					<b>{data.celebThree}</b>, the Cleveland 
+					<b>{data.adjectiveFor}</b>-stop. Here's the pitch... and it's hit... a short ground ball to third 
+					<b>{data.nounThree}</b>. 
+					<b>{data.celebFour}</b> scoops it up and throws it to first base for an out, and the game is over. And the Yankees move into second place in the 
+					<b>{data.adjectiveFive}</b> League! 
+				</p>
 			</div>
 		)
 	}
--- src/components/home.js
+++ src/components/home.js
@@ -13,3 +13,4 @@ export default class Home extends Component {
     );
   }
 }
+
